ce839ab2d8be10484e89dacc1ac02d83e6440cfb
DeniValuev <valuev.denis91@mail.ru>
2024-08-05 23:05:18 +0300

    Module_5-master: add new Unit_1

---
+++ Module_5.Unit_1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Drawing;
+
+namespace Module_5.Unit_1
+{
+    internal class Program
+    {
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском языке с маленькой буквы");
+            var color = Console.ReadLine();
+
+            switch (color) 
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow");
+                    break;
+            }
+
+            return color;
+        }
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            var favColors = new string[3];
+            for (int i = 0; i < favColors.Length; i++)
+            {
+                favColors[i] = ShowColor();
+            }
+            Console.WriteLine("Ваши любимые цвета:");
+            foreach (var color in favColors)
+            {
+                Console.WriteLine(color);
+            }
+            int temp;
+            var array = GetArrayFromConsole();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
